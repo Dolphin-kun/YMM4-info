366d766675263f259b98190f14fd0f6d592fa862
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-01-21 19:21:41 +0900

    app.ts を更新

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,3 +1,3 @@
 export const ROOT_URL = process.env.NEXT_PUBLIC_ROOT_URL as string;
-export const SITE_NAME = 'Next.js + MDX';
-export const SITE_DESCRIPTION = 'description';
+export const SITE_NAME = 'YMM4情報サイト';
+export const SITE_DESCRIPTION = 'YMM4を使った編集方法や機能の紹介をするサイトです';
