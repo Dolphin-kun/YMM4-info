79bf4d7927bdee2e1cff221ae6f0e66ab28f79cd
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-01-23 19:25:24 +0900

    date.ts を更新

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,11 @@
-export const formatDate = (date: string) =>
-  new Date(date).toLocaleDateString('ja-JP', {
+export const formatDate = (date: string) => {
+  const [year, month, day] = date.split('-').map(Number);
+
+  const dateObj = new Date(year, month - 1, day);
+
+  return dateObj.toLocaleDateString('ja-JP', {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
   });
+};
