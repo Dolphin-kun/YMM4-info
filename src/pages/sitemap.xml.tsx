44784233e98e538bed513dccb465400577b9eebd
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-01-25 14:42:13 +0900

    sitemap.xml.tsx を更新

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -4,6 +4,8 @@ import { Readable } from 'stream';
 
 export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => {
   const xml = await generateSitemapXml(); // xmlコードを生成する処理
+  
+  console.log(xml)
 
   res.statusCode = 200;
   res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate'); // 24時間のキャッシュ
@@ -17,14 +19,13 @@ export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => 
 
 async function generateSitemapXml():string {
   // An array with your links
-  const links = [{ url: '/',  changefreq: 'daily', priority: 0.3  }]
+  const links = [{ url: '/',  changefreq: 'daily', priority: 0.3  }];
   // Create a stream to write to
-  const stream = new SitemapStream( { hostname: 'https://ymm4-info.vercel.app' } )
+  const stream = new SitemapStream( { hostname: 'https://ymm4-info.vercel.app' } );
   
   // Return a promise that resolves with your XML string
   return streamToPromise(Readable.from(links).pipe(stream)).then((data) =>
     data.toString()
-    console.log(data)
   );
 };
 
