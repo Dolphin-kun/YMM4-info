093b91629778a0bd7fed1054559ea71a62b76b42
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-01-25 15:33:12 +0900

    サイトマップ

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://ymm4-info.vercel.app",
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    outDir: './data',
+  };
