f625c685f0f74e1dd2883833c9d13dc1e4881b58
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-01-26 16:11:28 +0900

    fix

--- src/components/guide/GuideHeader.tsx
+++ src/components/guide/GuideHeader.tsx
@@ -25,6 +25,31 @@ export const GuideHeader = ({ onMobileMenuButtonClick }: Props) => {
           </Box>
           <HomeLink />
         </Stack>
+        <Stack
+          direction="row"
+          alignItems="center"
+          spacing={3}
+          sx={{ display: { xs: 'none', md: 'flex' } }}
+        >
+          {globalRoutes.map((route) => (
+            <Link key={route.label} href={route.path ?? '/'}>
+              <Box
+                component="span"
+                sx={{
+                  p: 1,
+                  fontWeight: 900,
+                  color: theme.palette.grey[800],
+                  transitionDuration: '200ms',
+                  ':hover': {
+                    color: theme.palette.grey[600],
+                  },
+                }}
+              >
+                {route.label}
+              </Box>
+            </Link>
+          ))}
+        </Stack>
       </Box>
     </Container>
   );
