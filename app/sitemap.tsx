2d34fc01a65fdd1ebbd28f08cd44781f28fd900d
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-01-26 15:13:58 +0900

    sitemap

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -1,11 +1,12 @@
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseURL = "https://ymm4-info.vercel.app";
   const lastModified = new Date();
 
   const staticPaths = [
     {
-      url: process.env.NEXT_PUBLIC_ROOT_URL,
+      url: baseURL,
       lastModified,
     },
   ];
