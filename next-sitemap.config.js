9f6c0463f0817a9ce35a1c82f0637e13e867af8d
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-01-26 16:02:30 +0900

    sitemap

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://ymm4-info.vercel.app/",
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  outDir: './src/pages',
-};
