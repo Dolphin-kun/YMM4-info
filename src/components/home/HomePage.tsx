5113b713799223080c961b18522f0346339e1ea1
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-01-27 02:26:45 +0900

    fix

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -85,7 +85,7 @@ export const HomePage = ({
             <br />
             テスト配置です。
           </Typography>
-          <LinkButton href="/Effects/overview">エフェクト一覧</LinkButton>
+          <LinkButton href="/Effects/overview/">エフェクト一覧</LinkButton>
         </Stack>
 
       </Box>
