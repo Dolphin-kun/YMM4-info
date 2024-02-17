b64430c95595fe9e85ecfc51fba0f0cbd9ff08a2
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-02-17 15:06:39 +0900

    動的にsitemap作成

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,36 +1,42 @@
-import { GetServerSidePropsContext } from 'next';
+const EXTERNAL_DATA_URL = process.env.NEXT_PUBLIC_ROOT_URL;
 
-export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => {
-  const xml = await generateSitemapXml();
+function generateSiteMap(posts) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   
+     ${posts
+       .map(({ id }) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+       </url>
+     `;
+       })
+       .join('')}
+   </urlset>
+ `;
+}
 
-  res.statusCode = 200;
-  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate'); // 24時間のキャッシュ
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(EXTERNAL_DATA_URL);
+  const posts = await request.json();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(posts);
+
   res.setHeader('Content-Type', 'text/xml');
-  res.end(xml);
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
 
   return {
     props: {},
   };
-};
+}
 
-async function generateSitemapXml():string {
-    let xml = `<?xml version="1.0" encoding="UTF-8"?>`;
-    xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
-    
-    // ここでurlを足していく
-    const { posts } = await getAllPosts();
-    posts.forEach((post) =>{
-      xml += `
-        <url>
-          <loc>${process.env.NEXT_PUBLIC_ROOT_URL}/${post.path}</loc>
-        </url>
-      `
-    })
-    
-    xml += `</urlset>`;
-    return xml;
-  }
-
-const Page = () => null;
-export default Page;
-
+export default SiteMap;
