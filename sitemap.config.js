2f7f5e5761910b3d3879b5ae0b482742bce5757f
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-02-17 15:54:04 +0900

    fix

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_ROOT_URL || 'https://ymm4-info.vercel.app',
+    siteUrl: 'https://ymm4-info.vercel.app',
     generateRobotsTxt: true,
+    outDir: './public',
   }
   
