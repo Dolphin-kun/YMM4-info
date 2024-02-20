b8679935f647626ccbaf9006e928d1c2e9a2acc0
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-02-21 01:40:23 +0900

    fix

--- src/pages/sitemap.xml/index.tsx
+++ src/pages/sitemap.xml/index.tsx
@@ -1,11 +1,11 @@
 import { GetServerSideProps } from 'next'
-import { getServerSideSitemapLegacy } from 'next-sitemap'
+import { getServerSideSitemapLegacy, ISitemapField } from 'next-sitemap'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
     const res = await fetch('https://ymm4-info.vercel.app/')
     const pageData = await res.json();
     console.log(res);
-    const fields = [];
+    const fields: ISitemapField[] = [];
 
     pageData.forEach((page) => {
         console.log(page)
