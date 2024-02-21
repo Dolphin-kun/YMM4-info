539dc808f2b8d45006a3a1a363cb253700dad4ba
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-02-21 14:51:33 +0900

    fix

--- src/pages/sitemap.xml/index.tsx
+++ src/pages/sitemap.xml/index.tsx
@@ -1,7 +1,11 @@
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import { getServerSideSitemapLegacy, ISitemapField } from 'next-sitemap'
 import { DefaultPage } from '@/components/default';
 import { Typography } from '@mui/material';
+
+type Props = {
+    error?: ErrorResponse;
+  };
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
     const res = await fetch('https://ymm4-info.vercel.app/')
@@ -23,11 +27,13 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     ctx.res.setHeader('Cache-Control', 'max-age=86400'); // 24時間キャッシュ
     return getServerSideSitemapLegacy(ctx, fields)
 }
-
-export default function Page() {
-    return (
+const SitemapPage: NextPage<Props> = ({ error }) => {
+    if (error?.statusCode) {
         <DefaultPage>
-          <Typography variant="h1">Error</Typography>
+         <Typography variant="h1">Error</Typography>
         </DefaultPage>
-      );
-}
+    } else {
+      return null;
+    }
+  };
+  export default SitemapPage;
