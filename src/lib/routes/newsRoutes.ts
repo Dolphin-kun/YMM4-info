5b7bfbd2012e21e005d664fb9e63d1c86f51379f
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-03-01 21:20:49 +0900

    fix

--- src/lib/routes/newsRoutes.ts
+++ src/lib/routes/newsRoutes.ts
@@ -1,13 +1,12 @@
-import { Routes } from './type'
+import { Routes } from './type';
 
 export const newsRoutes: Routes = [
-    {
-        label: 'アップデート',
-        pages: [
-            {
-                label: 'v4.26.0.0',
-                path: '/news/update/4.26.0.0'
-            }
-        ]
-    }
-]
+  {
+    label: 'アップデート情報',
+    path: '/news/update/4.26.0.0',
+  },
+  {
+    label: 'ガイド',
+    path: '/guide/overview',
+  }
+];
