3ea9aa246f7cb3bfbf280a2545b71361768dbe37
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-03-06 11:55:21 +0000

    add

---
+++ src/pages/Effects/[...slug].tsx
@@ -0,0 +1,45 @@
+import { GuidePage } from '@/components/guide';
+import { components } from '@/components/mdx';
+import { getAllPaths, getMdxBySlug } from '@/lib/mdx';
+import { MdxSource } from '@/types/mdx';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import { MDXRemote } from 'next-mdx-remote';
+import { NextParsedUrlQuery } from 'next/dist/server/request-meta';
+
+type Props = {
+  mdxSource: MdxSource;
+};
+
+export default function Page({ mdxSource }: Props) {
+  return (
+    <GuidePage>
+      <MDXRemote {...mdxSource} components={components} />
+    </GuidePage>
+  );
+}
+
+const BASE_PATH = 'Effects';
+
+type Params = NextParsedUrlQuery & {
+  slug: string[];
+};
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  return {
+    paths: getAllPaths(BASE_PATH),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  params,
+}: GetStaticPropsContext) => {
+  const slug = params?.slug as string[];
+  const mdxSource = await getMdxBySlug(BASE_PATH, slug);
+
+  return {
+    props: {
+      mdxSource,
+    },
+  };
+};
