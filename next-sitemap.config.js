7a2a5a54e3a54fd518c8c956a8c62f35e6ddd6e4
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-03-09 05:26:40 +0000

    fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_ROOT_URL,
-    generateRobotsTxt: true, // (optional)
-    // ...other options
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    outDir: './out',
   }
