7e25c84ce40670eecf4650e73f060ab9230c01d6
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-03-09 13:24:49 +0900

    fix

---
+++ src/pages/faq/[...slug].tsx
@@ -0,0 +1,45 @@
+import { FaqPage } from '@/components/faq';
+import { components } from '@/components/mdx';
+import { getAllPaths, getMdxBySlug } from '@/lib/mdx';
+import { MdxSource } from '@/types/mdx';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import { MDXRemote } from 'next-mdx-remote';
+import { NextParsedUrlQuery } from 'next/dist/server/request-meta';
+
+type Props = {
+  mdxSource: MdxSource;
+};
+
+export default function Page({ mdxSource }: Props) {
+  return (
+    <FaqPage>
+      <MDXRemote {...mdxSource} components={components} />
+    </FaqPage>
+  );
+}
+
+const BASE_PATH = 'faq';
+
+type Params = NextParsedUrlQuery & {
+  slug: string[];
+};
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  return {
+    paths: getAllPaths(BASE_PATH),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  params,
+}: GetStaticPropsContext) => {
+  const slug = params?.slug as string[];
+  const mdxSource = await getMdxBySlug(BASE_PATH, slug);
+
+  return {
+    props: {
+      mdxSource,
+    },
+  };
+};
