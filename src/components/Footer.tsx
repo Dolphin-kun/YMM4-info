eace523ca22be9da37fea14ff28008f5247e6a1f
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-05-04 00:20:57 +0900

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,7 @@ export const Footer = () => {
               <Link href="https://x.com/YMM4_info" aria-label="link to X(Twitter) Account">
                 <X sx={{ fontSize: 32 }} />
               </Link>
-              <Link href="https://discord.gg/De56CXbpm6" aria-label="link to Disocrd Server">
+              <Link href="https://discord.gg/De56CXbpm6" aria-label="link to Disocrd Server" sx={{marginTop: "4px"}}>
               <Image
                 src="/assets/discordIcon.png"
                 width={32}
